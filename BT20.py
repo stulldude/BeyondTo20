a330edb4744acac5b6f6a9af30e8f83f2963c930
William Stull II <williamstull@gmail.com>
2021-10-31 16:28:35 -0700

    Adds default user info to chromedriver. This allows one time login to chrome and speeds up the process to get to the logging of info. Still needs the ability to get the info from the log

--- BT20.py
+++ BT20.py
@@ -12,12 +12,16 @@ load_dotenv()
 
 TIMEOUT = 180
 PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+#you can automate login by putting username and password into a .env file
 login = os.getenv("USER")
 PW = os.getenv("PW")
+CHROME_USER_PATH = os.getenv("CHROME_DEFAULT_USER_PATH")
 print("your username is " + login)
 
 options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
+options.add_argument(f"user-data-dir={CHROME_USER_PATH}}")
+options.add_argument("--start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--disable-blink-features")
@@ -26,72 +30,97 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 driver = webdriver.Chrome(options=options, executable_path=PATH)
 
 driver.get("https://www.dndbeyond.com")
+main_page = driver.current_window_handle
+
+#Login process
 try:
-    login_link = WebDriverWait(driver, 10).until(
+    login_link = WebDriverWait(driver, 3).until(
         EC.presence_of_element_located((By.ID, "login-link"))
     )
-except:
-    print('cant find login link')
-login_link.send_keys(Keys.RETURN)
+    login_link.send_keys(Keys.RETURN)
+    try:
+        #OAUTH automation
+        #You can remove/ignore this section if you want to login manually
+        signin_with_google = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "signin-with-google"))
+        )
+        signin_with_google.send_keys(Keys.RETURN)
+        driver.implicitly_wait(1)
 
-try:
-    element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "signin-with-twitch"))
-    )
-except:
-    print('can not find twitch btn')
-element.send_keys(Keys.RETURN)
+        #gets the login page
+        for handle in driver.window_handles:
+            if handle != main_page:
+                login_page = handle
 
-try:
-    print('...finding username')
-    username = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "login-username"))
-    )
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "password-input"))
-    )
+        #switches to login window and selects first email
+        try:
+            driver.switch_to.window(login_page)
+            your_account_expected = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "lCoei"))
+            )
+            your_account_expected.click()
+            driver.switch_to_window(main_page)
+        except:
+            print("could not switch to login or could not find any emails to login to")
+
+        #login automation with user info from .env
+        # try:
+        #     print('...finding username')
+        #     username = WebDriverWait(driver, 10).until(
+        #         EC.presence_of_element_located((By.ID, "login-username"))
+        #     )
+        #     password = WebDriverWait(driver, 10).until(
+        #         EC.presence_of_element_located((By.ID, "password-input"))
+        #     )
+        # except:
+        #     print('can not find input boxes')
+        # print('')
+        # username.send_keys(login)
+        # password.send_keys(PW)
+    except:
+        print('can not find google btn')
 except:
-    print('can not find input boxes')
-print('')
-username.send_keys(login)
-password.send_keys(PW)
+    print('Can not find login link. You might already be logged in')
+
+
 
 try:
     element = WebDriverWait(driver, TIMEOUT).until(
         EC.title_is("D&D Beyond - An official digital toolset for Dungeons & Dragons (D&D) Fifth Edition (5e)")
     )
+    driver.get("https://www.dndbeyond.com/my-campaigns")
+    print(f"Select a campaign within {TIMEOUT/60 * 3} minute{"s" if TIMEOUT/3 > 1 else ""}")
 except:
-    print('{TIMEOUT/3} minute timeout')
-
-
-driver.get("https://www.dndbeyond.com/my-campaigns")
+    print(f'{TIMEOUT/3} minute timeout')
 
 try:
-    game_log_btn = WebDriverWait(driver, TIMEOUT*3).until(
+    game_log_btn = WebDriverWait(driver, TIMEOUT * 3).until(
         EC.presence_of_element_located((By.CLASS_NAME, "gamelog-button"))
     )
+    game_log_btn.send_keys(Keys.RETURN)
 except:
-    print('Please select your campaign within {TIMEOUT/3 * 3} minutes')
+    print(f'Please select your campaign within {TIMEOUT/60 * 3} minutes')
 
-game_log_btn.send_keys(Keys.RETURN)
 try:
     g_log = WebDriverWait(driver, 1000).until(
         EC.presence_of_element_located((By.CLASS_NAME, "GameLog_GameLogEntries__3oNPD"))
     )
+    g_log_list = g_log.find_elements(By.TAG_NAME, 'li')
+
+    print(len(g_log_list))
+
+    for roll in g_log_list:
+        container = roll.find_element(By.TAG_NAME, 'div')
+        divs = container.find_elements(By.TAG_NAME, 'div')
+        for div in divs:
+            try:
+                print(div.text)
+            except:
+                print("no text")
 except:
     print('unable to find path for log')
-g_log_list = g_log.find_elements(By.TAG_NAME, 'li')
 
-print(len(g_log_list))
 
-for roll in g_log_list:
-    container = roll.find_element(By.TAG_NAME, 'div')
-    divs = container.find_elements(By.TAG_NAME, 'div')
-    for div in divs:
-        try:
-            print(div.text)
-        except:
-            print("no text")
 print('done')
     # sender = roll.find_element(By.CLASS_NAME, "GameLogEntry_Sender_1nlKd").text
     # action = roll.find_element(By.CLASS_NAME, "DiceMessage_action__192Yv").text
