3cf054b3b4a0cdc33891141b76ccaf2ad1222580
William Stull II <williamstull@gmail.com>
2021-11-03 22:43:21 -0700

    Adds second chromedriver for roll20 printing. Still working out kinks

--- BT20.py
+++ BT20.py
@@ -17,6 +17,8 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 login = os.getenv("USER")
 PW = os.getenv("PW")
 CHROME_USER_PATH = os.getenv("CHROME_DEFAULT_USER_PATH")
+CHROME_USER_PATH_2 = os.getenv("CHROME_DEFAULT_USER_PATH_2")
+
 print("your username is " + login)
 
 options = webdriver.ChromeOptions()
@@ -27,31 +29,40 @@ options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--disable-blink-features")
 options.add_argument("--disable-blink-features=AutomationControlled")
 
+options2 = webdriver.ChromeOptions()
+options2.add_argument(f"user-data-dir={CHROME_USER_PATH_2}")
+options2.add_argument("--start-maximized")
+options2.add_experimental_option("excludeSwitches", ["enable-automation"])
+options2.add_experimental_option('useAutomationExtension', False)
+options2.add_argument("--disable-blink-features")
+options2.add_argument("--disable-blink-features=AutomationControlled")
+
 driver = webdriver.Chrome(options=options, executable_path=PATH)
+roll20_driver = webdriver.Chrome(options=options2, executable_path=PATH)
 
 driver.get("https://www.dndbeyond.com")
 main_page = driver.current_window_handle
 
-def roll_message():
-    print()
+def get_text_input():
+    textarea_div = WebDriverWait(roll20_driver, 10).until(
+    EC.presence_of_element_located((By.ID, "textchat-input"))
+    )
+    input_area = textarea_div.find_element(By.TAG_NAME, 'textarea')
+    return input_area
 
-#needs to be reworked to account for multiple rolls in quick succession
-# i.e. if it goes from 12 rolls to 15 rolls, print out divs[0, 1, 2]
-def roll_listener(g_log):
-    print("listener, engaged")
+def roll20():
+    roll20_driver.get("https://www.roll20.net")
+    print("Select a game")
+    get_text_input()
+    print(f"Now printing results to {roll20_dr.get_title()}")
+
+def roll_message(g_log, i):
     rolls = g_log.find_elements(By.TAG_NAME, 'li')
-    curr_rolls = len(rolls)
-    prev_rolls = curr_rolls
-
-    while curr_rolls == prev_rolls:
-        rolls = g_log.find_elements(By.TAG_NAME, 'li')
-        curr_rolls = len(rolls)
-        time.sleep(1)
-    time.sleep(2)
-    rolls = g_log.find_elements(By.TAG_NAME, 'li')
-    divs = rolls[0].find_elements(By.TAG_NAME, 'div')
-
+    if i != 0:
+        rolls[i].click()
+    divs = rolls[i].find_elements(By.TAG_NAME, 'div')
     try:
+
         message_container = divs[0].find_elements(By.TAG_NAME, 'div')
         name = message_container[0]
         message_container2 = message_container[1].find_elements(By.TAG_NAME, 'div')
@@ -59,16 +70,59 @@ def roll_listener(g_log):
         roll_info = message.find_elements(By.TAG_NAME, 'div')
         roll_info_2 = roll_info[0].find_elements(By.TAG_NAME, 'div')
         dice_rolls = roll_info_2[1].find_element(By.TAG_NAME, 'span')
-
-        print(f"Player: {name.text}")
-        print(f"Roll type: {roll_info_2[0].text}")
-        print(f"Dice & modifiers: {roll_info_2[2].text}")
-        print(f"Roll results: {dice_rolls.get_attribute('title')}")
-        print(f"Total: {roll_info[5].get_attribute('innerText')}\n")
+        try:
+            if dice_rolls.get_attribute('innerText') == '?':
+                while dice_rolls.get_attribute('innerText') == '?':
+                    time.sleep(1)
+                    dice_rolls = roll_info_2[1].find_element(By.TAG_NAME, 'span')
+                    print("waiting for roll...")
+                roll_message(g_log, i)
+            else:
+                name = f"Player: {name.text}"
+                roll_type = f"Roll type: {roll_info_2[0].text}"
+                dice = f"Roll type: {roll_info_2[0].text}"
+                rolls = f"Roll results: {dice_rolls.get_attribute('innerText')}"
+                total = f"Total: {roll_info[5].get_attribute('innerText')}\n"
+                print(name)
+                print(roll_type)
+                print(rolls)
+                print(total)
+                text_input = get_text_input()
+                text_input.send_keys(f"{name}\n{roll_type}\n{rolls}\n{total}")
+        except:
+            roll_message(g_log, i)
 
     except Exception as e:
         print(e)
-    roll_listener(g_log)
+
+#needs to be reworked to account for multiple rolls in quick succession
+# i.e. if it goes from 12 rolls to 15 rolls, print out divs[0, 1, 2]
+def roll_listener(g_log, prev_rolls):
+    print("listener, engaged")
+    rolls = g_log.find_elements(By.TAG_NAME, 'li')
+    curr_rolls = prev_rolls
+
+    while curr_rolls == prev_rolls:
+        rolls = g_log.find_elements(By.TAG_NAME, 'li')
+        curr_rolls = len(rolls)
+        time.sleep(1)
+    print("new roll found... powering up")
+    time.sleep(3)
+    rolls = g_log.find_elements(By.TAG_NAME, 'li')
+
+    while curr_rolls != len(rolls):
+        print("new roll found... powering up")
+        curr_rolls = len(rolls)
+        rolls = g_log.find_elements(By.TAG_NAME, 'li')
+        time.sleep(3)
+
+    print("energy restored! Heres your roll or rolls:")
+    difference = curr_rolls - prev_rolls
+    for i in range(difference):
+        print(i)
+        roll_message(g_log, difference - (i + 1))
+
+    roll_listener(g_log, prev_rolls + difference)
 
 
 #Login process
@@ -143,8 +197,10 @@ try:
         g_log = WebDriverWait(driver, 1000).until(
             EC.presence_of_element_located((By.CLASS_NAME, "GameLog_GameLogEntries__3oNPD"))
         )
-        driver.implicitly_wait(3)
-        roll_listener(g_log)
+        print('waiting 4 seconds to load previous rolls in campaign')
+        roll20()
+        time.sleep(4)
+        roll_listener(g_log, len(g_log.find_elements(By.TAG_NAME, 'li')))
     except Exception as e:
         print('unable to find path for log')
         print(e)
